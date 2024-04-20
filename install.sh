379a9073a1e28503c1a6ca96ebc79e83577ba8e9
Roman Likane <likane@arrival.com>
2024-04-21 01:27:35 +0200

    install generates all binaries

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@ make -C binutils clean install
 make -C SmallerC/v0100 clean install
 make -C SmallerC/v0100/ucpp
 cp SmallerC/v0100/ucpp/ucpp /opt/cpu5/bin/ucpp
+
+make -C alu clean all
+cd alu && ./gen.sh && cd ..
+
+make -C microcode clean all
+cd microcode && ./gen.sh cpu6.txt && cd ..
+
