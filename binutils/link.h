d97fc6f1e5392ba05866084cbcc657413c772ffe
Roman Likane <likane@arrival.com>
2024-05-13 18:33:27 +0200

    kernel works

--- binutils/link.h
+++ binutils/link.h
@@ -5,7 +5,7 @@
 
 void linker_init();
 void linker_add_image(uint8_t * image, uint8_t * label_vec, uint8_t * label_mask, uint16_t image_size, uint16_t label_vec_size, uint16_t label_mask_size);
-void linker_offset_labels(uint16_t bss_offset);
+void linker_offset_labels(uint16_t bss_offset, uint16_t start_offset);
 void linker_link();
 
 #endif
